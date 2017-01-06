ce068e2824ac767c84ae5b8ac0d0021cbea77be5
alexrao <rxp2013@163.com>
2017-01-06 19:41:19 +0800

    添加文件操作的一些功能

--- file/file_rw_del/main.java
+++ file/file_rw_del/main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Date;
 
 public class main
 {
@@ -9,19 +10,26 @@ public class main
             File file = new File("test.txt");
             if(true)
             {
+                // 文件写入创建
                 FileWriter pbuf = new FileWriter(file);
-
                 BufferedWriter fp = new BufferedWriter(pbuf);
-
-                fp.write("Test0\n testlinn2\n");
-
+                fp.write("Test0\n");
                 fp.close();
+
+
+                // 文件追加
+                pbuf = new FileWriter(file, true);
+                fp = new BufferedWriter(pbuf);
+                fp.write("TestLine2\n");
+                fp.close();
+
                 System.out.println("文件创建成功!");
             }
 
             if(true)
             {
 
+                // 文件读取
                 FileReader pr = new FileReader(file);
                 BufferedReader bufr = new BufferedReader(pr);
                 String buf;
@@ -38,15 +46,60 @@ public class main
 
             if(true)
             {
-                if(file.delete())
+                // 文件检测和文件长度
+                if(file.exists() && file.isFile())
                 {
-                    System.out.println(file.getName() + "文件删除成功");
+                    System.out.println("文件长度:" + file.length() + "Bytes");
                 }
-                else
+
+                // 文件重命名
+                File newfile = new File("newtest.txt");
+                if(file.renameTo(newfile))
                 {
-                    System.out.println(file.getName() + "文件删除失败");
+                    System.out.println("[" + file.getName() + "]已重命名文件为[" + newfile.getName() + "]");
+                }
+
+                System.out.println("设置权限为只读：" + newfile.setReadOnly()); // 设置文件为只读
+                System.out.println("文件是否可写：" + newfile.canWrite());
+
+                if(true)
+                {
+                    Long time = newfile.lastModified();
+                    Date date = new Date(time);
+                    System.out.println("文件最后的修改时间:" + date);
+                }
+
+                if(false)
+                {
+                    // 文件删除
+                    if(newfile.delete())
+                    {
+                        System.out.println(newfile.getName() + "文件删除成功");
+                    }
+                    else
+                    {
+                        System.out.println(newfile.getName() + "文件删除失败");
+                    }
                 }
             }
+
+            create_dir_tmp_file();
+        }
+        catch(IOException e)
+        {
+            System.out.println("IOException!");
+        }
+    }
+
+    static void create_dir_tmp_file()
+    {
+        File file = null;
+        File dir = new File("./");
+
+        try
+        {
+        file = File.createTempFile("test", ".txt", dir);
+        System.out.println(file.getPath());
         }
         catch(IOException e)
         {
